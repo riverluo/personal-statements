98449dc4977a336f6b6dae556b283ab73c3b25de
yyypasserby <nixingwuwei@gmail.com>
2016-02-09 11:52:07 +0800

    Update 2016-02-09T03:52:01.820Z

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
-var gulp = require('gulp'); 
-var ghPages = require('gulp-gh-pages'); 
-var exec = require('child_process').exec; 
+var gulp = require('gulp');
+var ghPages = require('gulp-gh-pages');
+var exec = require('child_process').exec;
 
 gulp.task('deploy', ['build'], function () {
   return gulp.src('./_book/**/*')
@@ -11,7 +11,7 @@ gulp.task('build', function (cb) {
   exec('gitbook build', function (err, stdout, stderr) {
     console.log(stdout);
     console.log(stderr);
-    cb(err); 
+    cb(err);
   });
 });
 
